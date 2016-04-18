72e0164b4a4ca92f4653af5ec87f8fbfb9f7b159
Jae Nwawe <jnwawe@syr.edu>
2016-04-18 09:25:53 -0400

    Fade In

--- app/src/main/java/jaenwawe/miniInventory/ActivityRecyclerView.java
+++ app/src/main/java/jaenwawe/miniInventory/ActivityRecyclerView.java
@@ -1,29 +1,42 @@
 package jaenwawe.miniInventory;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.transition.Fade;
+import android.transition.Slide;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageButton;
 
 import java.util.HashMap;
 
 public class ActivityRecyclerView extends AppCompatActivity implements FragmentRecyclerView.OnRowClick {
 
-    private Toolbar toolbar;
-    private SearchView search;
     RecyclerView mRecyclerView;
     ProductData productData;
+    ImageButton productImage;
+    private Toolbar toolbar;
+    private SearchView search;
 
   @Override
-    public void onItemSelected (HashMap<String, ?> Product, View sharedImage) {
+  public void onItemSelected(HashMap<String, ?> product, View sharedImage) {
 
         productData = new ProductData();
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.framelayout_container_recyclerview, Fragment_MasterDetail.newInstance(Product, sharedImage))
-                .addToBackStack(null)
-                .commit();
+      Fragment_MasterDetail details = Fragment_MasterDetail.newInstance(product, sharedImage);
+      details.setSharedElementEnterTransition(new DetailsTransition());
+      details.setEnterTransition(new Fade());
+      details.setExitTransition(new Fade());
+      details.setSharedElementReturnTransition(new DetailsTransition());
+
+      getSupportFragmentManager().beginTransaction()
+              .replace(R.id.framelayout_container_recyclerview, details)
+              .addToBackStack(null)
+              .commit();
     }
 
     @Override
@@ -37,8 +50,16 @@ public class ActivityRecyclerView extends AppCompatActivity implements FragmentR
         setSupportActionBar(toolbar);
 
         if (savedInstanceState == null) {
+
+            FragmentRecyclerView fragment = FragmentRecyclerView.newInstance(0);
+            fragment.setEnterTransition(new Slide(Gravity.BOTTOM));
+            AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(getApplicationContext(),
+                    R.animator.fancy_animation);
+            set.setTarget(productImage);
+            set.start();
+
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.framelayout_container_recyclerview, FragmentRecyclerView.newInstance(0))
+                    .add(R.id.framelayout_container_recyclerview, fragment)
                     .commit();
         }
     }
